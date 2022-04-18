c1562fd779af1bd60ee87081d4d9886c4912de62
yassinemiladi21 <you@example.com>
2022-04-18 10:15:28 +0100

    ajout fichiers

---
+++ insc.js
@@ -0,0 +1,19 @@
+function change1() {
+  const signup = document.querySelector(".signup");
+  const login = document.querySelector(".login");
+  signup.style.opacity = "1";
+  signup.style.transition = "all 1s";
+  login.style.opacity = "0";
+  login.style.transition = "all 1s";
+  login.style.zIndex = "-1";
+}
+
+function change2() {
+  const signup = document.querySelector(".signup");
+  const login = document.querySelector(".login");
+  login.style.opacity = "1";
+  login.style.transition = "all 1s";
+  login.style.zIndex = "1";
+  signup.style.opacity = "0";
+  signup.style.transition = "all 1s";
+}
